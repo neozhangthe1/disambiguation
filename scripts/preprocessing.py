29256eab8b9f3f517e75a14c96883c1d4c3e6153
Fanjin Zhang <zfjsail@163.com>
2018-06-04 19:37:58 +0800

    use mp Process

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,11 +1,58 @@
 from os.path import join
+import multiprocessing as mp
 from global_.embedding import EmbeddingModel
+from datetime import datetime
 from utils.cache import LMDBClient
 from utils import data_utils
 from utils import feature_utils
 from utils import settings
 
 global_dir = join(settings.DATA_DIR, 'global')
+start_time = datetime.now()
+
+
+def put_papers(task_q, N_PROC):
+    for i, paper in enumerate(data_utils.pubs_load_generator()):
+        task_q.put(paper)
+    for _ in range(N_PROC):
+        task_q.put(None)
+
+
+def cal_author_features(task_q, feature_q):
+    while True:
+        paper = task_q.get()
+        if paper is None:
+            feature_q.put((None, None))
+            break
+        n_authors = len(paper.get('authors', []))
+        author_features = [feature_utils.extract_author_features(paper, j) for j in range(n_authors)]
+        for j in range(n_authors):
+            feature_q.put(('{}-{}'.format(paper['sid'], j), author_features[j]))
+
+
+def dump_author_features():
+    N_PROC = 16
+
+    task_q = mp.Queue(N_PROC * 6)
+    feature_q = mp.Queue(1000)
+
+    producer_p = mp.Process(target=put_papers, args=(task_q, N_PROC))
+    consumer_ps = [mp.Process(target=cal_author_features, args=(task_q, feature_q)) for _ in range(N_PROC)]
+    producer_p.start()
+    [p.start() for p in consumer_ps]
+
+    cnt = 0
+    LMDB_NAME = 'pub_authors.feature'
+    lc = LMDBClient(LMDB_NAME)
+
+    while True:
+        if cnt % 100 == 0:
+            print('paper cnt', cnt, datetime.now()-start_time)
+        pid_order, feature = feature_q.get()
+        if pid_order is None:
+            break
+        lc.set(pid_order, feature)
+        cnt += 1
 
 
 def dump_author_features_lmdb():
@@ -31,4 +78,6 @@ def dump_author_features_lmdb():
 
 
 if __name__ == '__main__':
-    dump_author_features_lmdb()
+    # dump_author_features_lmdb()
+    dump_author_features()
+    print('done')
